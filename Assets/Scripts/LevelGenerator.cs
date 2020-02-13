f1404ffb8f97d322b4ee9f9597314d6fcddcd7d7
Ben Ritson <benjamin.ritson@gmail.com>
2020-02-13 06:20:19 +0000

    Working out some kinks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,24 +4,65 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
-    private Track track;
+    private Track track;    
+
+    GameObject levelFloor;
+    GameObject levelBar;
     private float levelLength;
-    private string levelFloorString = "levelFloor";
+
+    //Interpolation Variables
+    private float lerpTime;
+    private float lerpLength;
+
+    Vector3 levelBarStartPos;
+    Vector3 levelBarEndPos;
+
+    private string levelFloorNameString = "levelFloor";
+    private string levelBarNameString = "levelBar";
 
     public void GenerateLevel(Track _track)
     {
-        if (gameObject.transform.Find(levelFloorString))
+        
+        if (gameObject.transform.Find(levelFloorNameString))
         {
             Destroy(gameObject.transform.Find("levelFloor").gameObject);
-        }       
+        }
+        lerpLength = 0;
+            
 
         track = _track;
         levelLength = _track.GetTrackLength();
+        lerpTime = 0;
 
-        GameObject levelFloor = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        levelFloor.transform.SetParent(transform);
-        levelFloor.name = levelFloorString;
+        //Create Floor
+        levelFloor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        levelFloor.name = levelFloorNameString;
         levelFloor.transform.position = new Vector3(0, 0, 0);
-        levelFloor.transform.localScale = new Vector3(levelLength, 1, 1);        
+        levelFloor.transform.localScale = new Vector3(levelLength, 1, 1);
+        levelFloor.transform.SetParent(transform); //Make sure to set parent after transform and position have been set :)
+
+        //Add and Create Material and Shader
+        Shader shader = Shader.Find("Standard");
+        Material floorMaterial = new Material(shader);
+        floorMaterial.color = Color.red;
+        levelFloor.GetComponent<Renderer>().material = floorMaterial;
+
+        levelBar = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        levelBar.name = levelBarNameString;
+        float barWidth = levelLength / 20;
+        levelBarStartPos = levelFloor.transform.position + new Vector3(-levelLength / 2 + barWidth / 2, 0, -1);        
+        levelBarEndPos = levelFloor.transform.position + new Vector3(levelLength / 2 - barWidth / 2, 0, -1);        
+        levelBar.transform.localScale = new Vector3(barWidth, 1, 1);
+        levelBar.transform.SetParent(levelFloor.transform);
+
+        lerpTime = Time.time;
+        lerpLength = Vector3.Distance(levelBarStartPos, levelBarEndPos);
+        levelBar.transform.position = levelBarStartPos;
+    }
+
+    public void Update()
+    {
+        lerpTime += Time.deltaTime / levelLength;
+        levelBar.transform.position = Vector3.Lerp(levelBarStartPos, levelBarEndPos, lerpTime);
     }
 }
