9b62cae5a3b5a79269e7c71ccc2ecd4264f63762
Ben Ritson <benjamin.ritson@gmail.com>
2020-03-13 00:44:21 +0000

    Sick background

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -9,6 +9,8 @@ public class MusicManager : MonoBehaviour {
 
     public float test;
 
+    public bool comparison;
+
     //Track loading and playback
     private Object[] objectsToLoad; // Loading in music tracks
     private List<Track> loadedAudioTracks = new List<Track>();
@@ -26,29 +28,40 @@ public class MusicManager : MonoBehaviour {
     //Visualiser Variables
     public GameObject visualiserCubePrefab;
     public GameObject visualiserHolder;
-    GameObject[] visualiserCubes = new GameObject[59];
+    GameObject[] visualiserCubes = new GameObject[64];  //if using 64 bands, set to 59 as the last 5 never really get hit
+    Color[] currentCubeColor = new Color[64];
     public float startScale;
     public float scaleMultiplier;
 
-    //Audio Frequency data stuff
-    const int bands = 8;
-    
+    [Range(0,1)]
+    public float cutoff;
+
+    //Audio Frequency data stuff    
     float[] spectrumDataLeft = new float[512];
     float[] spectrumDataRight = new float[512];
 
+    //8 bands
+    float[] freqencyBand8 = new float[8];
+    float[] bandBuffer8 = new float[8];
+    float[] bufferDecrease8 = new float[8];
+    float[] frequencyBandHighest8 = new float[8];
+
     //64 bands
-    float[] freqencyBand = new float[64];    
-    float[] bandBuffer = new float[64];
-    float[] bufferDecrease = new float[64];
-    float[] frequencyBandHighest = new float[64];
+    float[] freqencyBand64 = new float[64];    
+    float[] bandBuffer64 = new float[64];
+    float[] bufferDecrease64 = new float[64];
+    float[] frequencyBandHighest64 = new float[64];
 
     public float decrease;
     public float increase;
     public bool useBuffer;
 
     //Normalised Frequency Data
-    float[] frequencyBandNormalised = new float[64];  //Use this in gameplay and mechanics when not using buffer.
-    float[] bandBufferNormalised = new float[64];  //Use this in gameplay and mechanics when using buffer.      
+    float[] frequencyBandNormalised8 = new float[8];  //Use this in gameplay and mechanics when not using buffer.
+    float[] bandBufferNormalised8 = new float[8];  //Use this in gameplay and mechanics when using buffer.   
+
+    float[] frequencyBandNormalised64 = new float[64];  
+    float[] bandBufferNormalised64 = new float[64];      
 
     //Beat Detection Variables
 
@@ -143,6 +156,7 @@ public class MusicManager : MonoBehaviour {
     {
         float maxWidth = Vector2.Distance(cam.ScreenToWorldPoint(new Vector2(0, 0)), cam.ScreenToWorldPoint(new Vector2(0, cam.pixelWidth)));
         float barWidth = maxWidth / visualiserCubes.Length;
+        float barHeight = Vector2.Distance(cam.ScreenToWorldPoint(new Vector2(0, 0)), cam.ScreenToWorldPoint(new Vector2(0, cam.pixelHeight)));
         if(visualiserHolder != null)
         {
             DestroyImmediate(visualiserHolder);
@@ -158,7 +172,7 @@ public class MusicManager : MonoBehaviour {
         for (int i = 0; i < visualiserCubes.Length; i++)
         {
             GameObject cube = (GameObject)Instantiate(visualiserCubePrefab);
-            cube.transform.localScale = new Vector3(barWidth, 1, 1);
+            cube.transform.localScale = new Vector3(barWidth, barHeight, 1);
             cube.transform.position = prevPos + new Vector3(barWidth, 0, 0);
             prevPos = cube.transform.position;
             cube.transform.parent = visualiserHolder.transform;
@@ -166,8 +180,10 @@ public class MusicManager : MonoBehaviour {
 
             Shader shader = Shader.Find("Standard");
             Material mat = new Material(shader);
+
             mat.color = Color.green;
             cube.GetComponent<Renderer>().material = mat;
+            cube.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
 
 
             visualiserCubes[i] = cube;
@@ -211,60 +227,181 @@ public class MusicManager : MonoBehaviour {
             currentSong.source.GetSpectrumData(spectrumDataLeft, 0, window);
             currentSong.source.GetSpectrumData(spectrumDataRight, 1, window);
 
-            CreateFrequencyBands();
-            BandBuffer();
-            CreateAudioBands();
+            CreateFrequencyBands8();
+            BandBuffer8();
+            CreateAudioBands8();
+
+            CreateFrequencyBands64();
+            BandBuffer64();
+            CreateAudioBands64();            
 
             for (int i = 0; i < visualiserCubes.Length; i++)
             {
+                float iNormal = ((float)i / visualiserCubes.Length) * 0.8f; //times 0.75 to make it a scale of 0 - 0.75
                 if (visualiserCubes != null)
-                {
+                {           
+                    
                     if (useBuffer)
                     {
-                        visualiserCubes[i].transform.localScale = new Vector3(visualiserCubes[i].transform.localScale.x, (bandBuffer[i] * scaleMultiplier) + startScale, 1);
+                        Color color;
+                        if (comparison)
+                        {
+                            
+                            if (frequencyBandNormalised64[i] > cutoff)
+                            {
+                                currentCubeColor[i] = Color.HSVToRGB(iNormal, 0.9f, Mathf.Clamp(bandBufferNormalised64[i], 0, 0.7f));
+                            }
+                            else
+                            {
+                                currentCubeColor[i] -= Color.HSVToRGB(0, 0f, 0.05f);
+                            }
+
+
+                        } else
+                        {
+
+                            if (frequencyBandNormalised64[i] > cutoff)
+                            {
+                                currentCubeColor[i] = Color.HSVToRGB(iNormal, 0.9f, Mathf.Clamp(bandBufferNormalised64[i], 0, 0.7f));
+                            }
+                            else
+                            {
+                                currentCubeColor[i] = Color.HSVToRGB(iNormal, 0f, 0.05f);
+                            }
+
+                        }
+                        
+                        visualiserCubes[i].GetComponent<Renderer>().material.color = currentCubeColor[i];
+                        visualiserCubes[i].GetComponent<Renderer>().material.SetColor("_EmissionColor", currentCubeColor[i]);
+                        
+                        //visualiserCubes[i].transform.localScale = new Vector3(visualiserCubes[i].transform.localScale.x, (bandBuffer[i] * scaleMultiplier) + startScale, 1);
                     } else
                     {
-                        visualiserCubes[i].transform.localScale = new Vector3(visualiserCubes[i].transform.localScale.x, (freqencyBand[i] * scaleMultiplier) + startScale, 1);
+                        //print(bandBufferNormalised[i] * 255);
+                        Color color;
+                        if (frequencyBandNormalised64[i] > cutoff)
+                        {
+                            color = Color.HSVToRGB(iNormal, 0.9f, Mathf.Clamp(frequencyBandNormalised64[i], 0, 0.7f));
+                        }
+                        else
+                        {
+                            color = Color.HSVToRGB(iNormal, 0.9f, 0.05f);
+                        }
+
+                        visualiserCubes[i].GetComponent<Renderer>().material.color = color;
+                        visualiserCubes[i].GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+
+                        //visualiserCubes[i].transform.localScale = new Vector3(visualiserCubes[i].transform.localScale.x, (freqencyBand[i] * scaleMultiplier) + startScale, 1);
                     }
                 }
             }
         }
     }
 
-    void CreateAudioBands()
+    void CreateAudioBands8()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            //Set the band roof to the highest the band has been.
+            if (freqencyBand8[i] > frequencyBandHighest8[i])
+            {
+                frequencyBandHighest8[i] = freqencyBand8[i];
+            }
+
+            frequencyBandNormalised8[i] = (freqencyBand8[i] / frequencyBandHighest8[i]);
+            bandBufferNormalised8[i] = (bandBuffer8[i] / frequencyBandHighest8[i]);
+
+        }
+    }
+
+    void BandBuffer8()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (freqencyBand8[i] > bandBuffer8[i])
+            {
+                bandBuffer8[i] = freqencyBand8[i];
+                bufferDecrease8[i] = decrease;
+
+            }
+            else if (freqencyBand8[i] < bandBuffer8[i])
+            {
+                bandBuffer8[i] -= bufferDecrease8[i];
+                bufferDecrease8[i] *= increase;
+            }
+        }
+    }
+
+    void CreateFrequencyBands8()
+    {
+        /* 
+        20-60 - Sub Bass
+        60-250 - Bass
+        250-500 - Low Midrange 
+        500-2000 - Midrange
+        2000-4000 - Upper Midrange
+        4000-6000 - Presence
+        6000-20000 - Brilliance
+        */
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) //algorithm by peer play
+        {
+            float average = 0;
+            int sampleBand = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {  
+               sampleBand += 2;
+            }
+
+            for (int j = 0; j < sampleBand; j++)
+            {
+                average += spectrumDataLeft[count] + spectrumDataRight[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqencyBand8[i] = average * 10;
+        }
+    }
+
+    void CreateAudioBands64()
     {
         for(int i = 0; i < 64; i++)
         {
             //Set the band roof to the highest the band has been.
-            if(freqencyBand[i] > frequencyBandHighest[i])
+            if(freqencyBand64[i] > frequencyBandHighest64[i])
             {
-                frequencyBandHighest[i] = freqencyBand[i];
+                frequencyBandHighest64[i] = freqencyBand64[i];
             }
 
-            frequencyBandNormalised[i] = (freqencyBand[i] / frequencyBandHighest[i]);            
-            bandBufferNormalised[i] = (bandBuffer[i] / frequencyBandHighest[i]);
+            frequencyBandNormalised64[i] = (freqencyBand64[i] / frequencyBandHighest64[i]);            
+            bandBufferNormalised64[i] = (bandBuffer64[i] / frequencyBandHighest64[i]);
 
         }
     }
 
-    void BandBuffer()
+    void BandBuffer64()
     {
         for(int i = 0; i < 64; i++)
         {
-            if(freqencyBand[i] > bandBuffer[i])
+            if(freqencyBand64[i] > bandBuffer64[i])
             {
-                bandBuffer[i] = freqencyBand[i];
-                bufferDecrease[i] = decrease;
+                bandBuffer64[i] = freqencyBand64[i];
+                bufferDecrease64[i] = decrease;
 
-            } else if (freqencyBand[i] < bandBuffer[i])
+            } else if (freqencyBand64[i] < bandBuffer64[i])
             {
-                bandBuffer[i] -= bufferDecrease[i];
-                bufferDecrease[i] *= increase;
+                bandBuffer64[i] -= bufferDecrease64[i];
+                bufferDecrease64[i] *= increase;
             }
         }
     }
 
-    void CreateFrequencyBands()
+    void CreateFrequencyBands64()
     {
         /*
         20-60 - Sub Bass
@@ -280,7 +417,7 @@ public class MusicManager : MonoBehaviour {
         int sampleBand = 1;
         int power = 0;
 
-        for(int i = 0; i < 64; i++)
+        for(int i = 0; i < 64; i++) //algorithm by peer play
         {
             float average = 0;
 
@@ -302,7 +439,7 @@ public class MusicManager : MonoBehaviour {
 
             average /= count;
 
-            freqencyBand[i] = average * 80;
+            freqencyBand64[i] = average * 80;
         }
-    }
+    }    
 }
